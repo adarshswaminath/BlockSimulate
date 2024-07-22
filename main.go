00dfd1cad63f9690ba1cf6b63707a87ca44c254c
Adarsh <69946307+adarshswaminath@users.noreply.github.com>
2024-07-22 09:39:00 +0530

    feat: api endpoints created using the Fiber

--- main.go
+++ main.go
@@ -3,9 +3,12 @@ package main
 import (
 	"fmt"
 	"time"
+
+	"github.com/gofiber/fiber/v2"
 )
 
 func main() {
+
 	genesisBlock := Block{
 		Index:        0,
 		Timestamp:    time.November.String(),
@@ -17,29 +20,29 @@ func main() {
 	genesisBlock.Hash = CalculateHash(genesisBlock)
 	blockchain := []Block{genesisBlock}
 
-	// newBlock := CreateBlock(genesisBlock, "second Block")
-	// blockchain = append(blockchain, newBlock)
+	// fiber setup
+	app := fiber.New()
+	go func() {
+		for {
+			previousBlock := blockchain[len(blockchain)-1]
+			newData := fmt.Sprintf("Block #%d", previousBlock.Index+1)
+			newBlock := CreateBlock(previousBlock, newData)
 
-	// newBlock1 := CreateBlock(newBlock, "second Block")
-	// blockchain = append(blockchain, newBlock1)
+			blockchain = append(blockchain, newBlock)
+			// Sleep for a while to simulate time between block creation
+			time.Sleep(2 * time.Second)
+		}
+	}()
 
-	for {
-		previousBlock := blockchain[len(blockchain)-1]
-		newData := fmt.Sprintf("Block #%d", previousBlock.Index+1)
-		newBlock := CreateBlock(previousBlock, newData)
+	// API endpoints
+	app.Get("/block-data", func(c *fiber.Ctx) error {
+		return c.JSON(blockchain)
+	})
 
-		blockchain = append(blockchain, newBlock)
-		fmt.Println(blockchain)
+	app.Get("/latest-block", func(c *fiber.Ctx) error {
+		return c.JSON(blockchain[len(blockchain)-1])
+	})
 
-		// Print the new block
-		// fmt.Printf("Index: %d\n", newBlock.Index)
-		// fmt.Printf("Timestamp: %s\n", newBlock.Timestamp)
-		// fmt.Printf("Data: %s\n", newBlock.Data)
-		// fmt.Printf("PreviousHash: %s\n", newBlock.PreviousHash)
-		// fmt.Printf("Hash: %s\n\n", newBlock.Hash)
-
-		// Sleep for a while to simulate time between block creation
-		time.Sleep(2 * time.Second)
-	}
+	app.Listen(":3000")
 
 }
