ac154864d7aa5ab804800e8f150f4a7de0dda8fb
adarshswaminath <adarshswaminath7@gmail.com>
2024-08-04 18:44:06 +0530

    fix: navbar mobile screen issue

--- frontend/src/Components/Navbar.tsx
+++ frontend/src/Components/Navbar.tsx
@@ -1,4 +1,4 @@
-import { FaGithub } from "react-icons/fa"
+import { FaGithub, FaWallet } from "react-icons/fa"
 import {Link} from "react-router-dom"
 export default function Navbar() {
   return (
@@ -8,7 +8,10 @@ export default function Navbar() {
        </Link>
         <div className="flex items-center space-x-2">
         <Link to="/wallet">
-        <button className="btn w-32 bg-white text-black hover:text-white rounded-full tracking-wider">Wallet</button>
+        <button className="btn lg:w-32 bg-white text-black hover:text-white rounded-full tracking-wider">
+          <span className="lg:block hidden">Wallet</span>
+          <span className="block text-lg lg:hidden"><FaWallet /></span>
+        </button>
         </Link>
         <a href="https://github.com/adarshswaminath/BlockSimulate" target="_blank" className="btn rounded-full text-xl bg-white text-black hover:text-white">
         <FaGithub />
